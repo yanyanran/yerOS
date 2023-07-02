b1203c00d599c7edc1329fb388671a97ae5251da
yanyanran <yanran66666@qq.com>
2023-07-02 13:32:43 +0800

    add系统调用malloc+free

--- kernel/main.c
+++ kernel/main.c
@@ -20,15 +20,16 @@ int prog_b_pid = 0;
 int main(void) {
   put_str("I am kernel\n");
   init_all();
-
-  // process_execute(u_prog_a, "user_prog_a");
-  // process_execute(u_prog_b, "user_prog_b");
-
   intr_enable(); // 开中断
   // console_put_str("main_pid:0x");
   // console_put_int(sys_getpid());
   // console_put_char('\n');
 
+  // 用户进程独享内存空间，虚拟地址相同不冲突
+  process_execute(u_prog_a, "user_prog_a");
+  process_execute(u_prog_b, "user_prog_b");
+
+  // 内核线程共享内存空间，虚拟地址唯一
   thread_start("k_thread_a", 31, k_thread_a, "I am thread_a");
   thread_start("k_thread_b", 31, k_thread_b, "I am thread_b ");
 
@@ -40,115 +41,77 @@ int main(void) {
 
 // 线程中运行的函数
 void k_thread_a(void *arg) {
-  // char *para = arg;
-  void *addr1;
-  void *addr2;
-  void *addr3;
-  void *addr4;
-  void *addr5;
-  void *addr6;
-  void *addr7;
-  console_put_str(" thread_a start\n");
-  int max = 1000;
-  while (max-- > 0) {
-    int size = 128;
-    addr1 = sys_malloc(size);
-    size *= 2;
-    addr2 = sys_malloc(size);
-    size *= 2;
-    addr3 = sys_malloc(size);
-    sys_free(addr1);
-    addr4 = sys_malloc(size);
-    size *= 2;
-    size *= 2;
-    size *= 2;
-    size *= 2;
-    size *= 2;
-    size *= 2;
-    size *= 2;
-    addr5 = sys_malloc(size);
-    addr6 = sys_malloc(size);
-    sys_free(addr5);
-    size *= 2;
-    addr7 = sys_malloc(size);
-    sys_free(addr6);
-    sys_free(addr7);
-    sys_free(addr2);
-    sys_free(addr3);
-    sys_free(addr4);
-  }
-  console_put_str(" thread_a end\n");
+  void *addr1 = sys_malloc(256);
+  void *addr2 = sys_malloc(255);
+  void *addr3 = sys_malloc(254);
+  console_put_str(" thread_a malloc addr:0x");
+  console_put_int((int)addr1);
+  console_put_char(',');
+  console_put_int((int)addr2);
+  console_put_char(',');
+  console_put_int((int)addr3);
+  console_put_char('\n');
+
+  int cpu_delay = 100000;
+  while (cpu_delay-- > 0)
+    ;
+  sys_free(addr1);
+  sys_free(addr2);
+  sys_free(addr3);
   while (1)
     ;
 }
 
 void k_thread_b(void *arg) {
-  //char *para = arg;
-  void *addr1;
-  void *addr2;
-  void *addr3;
-  void *addr4;
-  void *addr5;
-  void *addr6;
-  void *addr7;
-  void *addr8;
-  void *addr9;
-  int max = 1000;
-  console_put_str(" thread_b start\n");
-  while (max-- > 0) {
-    int size = 9;
-    addr1 = sys_malloc(size);
-    size *= 2;
-    addr2 = sys_malloc(size);
-    size *= 2;
-    sys_free(addr2);
-    addr3 = sys_malloc(size);
-    sys_free(addr1);
-    addr4 = sys_malloc(size);
-    addr5 = sys_malloc(size);
-    addr6 = sys_malloc(size);
-    sys_free(addr5);
-    size *= 2;
-    addr7 = sys_malloc(size);
-    sys_free(addr6);
-    sys_free(addr7);
-    sys_free(addr3);
-    sys_free(addr4);
-    size *= 2;
-    size *= 2;
-    size *= 2;
-    addr1 = sys_malloc(size);
-    addr2 = sys_malloc(size);
-    addr3 = sys_malloc(size);
-    addr4 = sys_malloc(size);
-    addr5 = sys_malloc(size);
-    addr6 = sys_malloc(size);
-    addr7 = sys_malloc(size);
-    addr8 = sys_malloc(size);
-    addr9 = sys_malloc(size);
-    sys_free(addr1);
-    sys_free(addr2);
-    sys_free(addr3);
-    sys_free(addr4);
-    sys_free(addr5);
-    sys_free(addr6);
-    sys_free(addr7);
-    sys_free(addr8);
-    sys_free(addr9);
-  }
-  console_put_str(" thread_b end\n");
+  void *addr1 = sys_malloc(256);
+  void *addr2 = sys_malloc(255);
+  void *addr3 = sys_malloc(254);
+  console_put_str(" thread_b malloc addr:0x");
+  console_put_int((int)addr1);
+  console_put_char(',');
+  console_put_int((int)addr2);
+  console_put_char(',');
+  console_put_int((int)addr3);
+  console_put_char('\n');
+
+  int cpu_delay = 100000;
+  while (cpu_delay-- > 0)
+    ;
+  sys_free(addr1);
+  sys_free(addr2);
+  sys_free(addr3);
   while (1)
     ;
 }
 
 void u_prog_a(void) {
-  printf(" prog_a_pid:0x%x\n", getpid());
+  void *addr1 = malloc(256);
+  void *addr2 = malloc(255);
+  void *addr3 = malloc(254);
+  printf(" prog_a malloc addr:0x%x,0x%x,0x%x\n", (int)addr1, (int)addr2,
+         (int)addr3);
+  int cpu_delay = 100000;
+  while (cpu_delay-- > 0)
+    ;
+  free(addr1);
+  free(addr2);
+  free(addr3);
   while (1)
     ;
 }
 
 void u_prog_b(void) {
-  printf(" prog_b_pid:0x%x\n", getpid());
+  void *addr1 = malloc(256);
+  void *addr2 = malloc(255);
+  void *addr3 = malloc(254);
+  printf(" prog_b malloc addr:0x%x,0x%x,0x%x\n", (int)addr1, (int)addr2,
+         (int)addr3);
+  int cpu_delay = 100000;
+  while (cpu_delay-- > 0)
+    ;
+  free(addr1);
+  free(addr2);
+  free(addr3);
   while (1)
     ;
 }
--- lib/user/syscall.h
+++ lib/user/syscall.h
@@ -2,8 +2,15 @@
 #define LIB_USER_SYSCALL
 
 #include "stdint.h"
-enum SYSCALL_NR { SYS_GETPID, SYS_WRITE }; // 枚举结构存放系统调用子功能号
+enum SYSCALL_NR {
+  SYS_GETPID,
+  SYS_WRITE,
+  SYS_MALLOC,
+  SYS_FREE
+}; // 枚举结构存放系统调用子功能号
 uint32_t getpid(void);
 uint32_t write(char *str);
+void *malloc(uint32_t size);
+void free(void *ptr);
 
 #endif /* LIB_USER_SYSCALL */
